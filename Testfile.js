fb4d0889cd3e1fa6a1b5b097e7dd32efaf4589c3
Nico Gomez <nicolasgomez1994@gmail.com>
2022-06-27 02:31:49 -0400

    successful refactor of constructor function

--- Testfile.js
+++ Testfile.js
@@ -1,14 +1,9 @@
-// const Rotor = require("./Rotor.js");
-// const Plugboard = require("./Plugboard");
-// const Reflector = require("./Reflector");
-// // const EnigmaMachine = require("./EnigmaMachine");
-
 import { Rotor } from "./Rotor.js";
 import { Plugboard } from "./Plugboard.js";
 import { Reflector } from "./Reflector.js";
 import { EnigmaMachine } from "./EnigmaMachine.js";
 
-const plugBoard = new Plugboard();
+const plugboard = new Plugboard(["AB", "CD", "EF"]);
 
 //Rotor wirings: Input ABCDEFGHIJKLMNOPQRSTUVWXYZ -> output
 const rotorIwiring = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"; // startPos= A, turnover on A
@@ -20,55 +15,33 @@ const rotorVIwiring = "JPGVOUMFYQBENHZRDKASXLICTW"; // startPos= A, turnover on 
 const rotorVIIwiring = "NZJHGRCXMYSWBOUFAIVLPEKQDT"; // startPos= A, turnover on A
 const rotorVIIIwiring = "FKQHTLXOCBJSPDZRAMEWNIUYGV"; // startPos= A, turnover on A
 
-const RotorI = new Rotor();
-RotorI.setWiringTable(rotorIwiring);
-RotorI.setStartPosition("A");
-RotorI.setTurnoverLetter("R");
-
-const RotorII = new Rotor();
-RotorII.setWiringTable(rotorIIwiring);
-RotorI.setStartPosition("A");
-RotorII.setTurnoverLetter("F");
-
-const RotorIII = new Rotor();
-RotorIII.setWiringTable(rotorIIIwiring);
-RotorI.setStartPosition("A");
-RotorIII.setTurnoverLetter("W");
-
-const RotorIV = new Rotor();
-RotorIV.setWiringTable(rotorIVwiring);
-RotorI.setStartPosition("A");
-RotorIV.setTurnoverLetter("K");
-
-const RotorV = new Rotor();
-RotorV.setWiringTable(rotorVwiring);
-RotorI.setStartPosition("A");
-RotorV.setTurnoverLetter("A");
+const RotorI = new Rotor(rotorIwiring, "A", "R");
+const RotorII = new Rotor(rotorIIwiring, "A", "F");
+const RotorIII = new Rotor(rotorIIIwiring, "A", "W");
 
 // Reflector Wirings: Input ABCDEFGHIJKLMNOPQRSTUVWXYZ
 const reflectorAwiring = "EJMZALYXVBWFCRQUONTSPIKHGD";
 const reflectorBwiring = "YRUHQSLDPXNGOKMIEBFZCWVJAT";
 const reflectorCwiring = "FVPJIAOYEDRZXWGCTKUQSBNMHL";
 
-const ReflectorA = new Reflector();
-ReflectorA.setReflectionPairs(reflectorAwiring);
+const ReflectorA = new Reflector(reflectorAwiring);
+const ReflectorB = new Reflector(reflectorBwiring);
+const ReflectorC = new Reflector(reflectorCwiring);
 
-const ReflectorB = new Reflector();
-ReflectorB.setReflectionPairs(reflectorBwiring);
-
-const ReflectorC = new Reflector();
-ReflectorC.setReflectionPairs(reflectorCwiring);
-
-const machine = new EnigmaMachine();
-machine.setPlugboard(plugBoard);
-machine.setRotors(RotorI, RotorII, RotorIII);
-machine.setReflector(ReflectorA);
+const machine = new EnigmaMachine(
+  plugboard,
+  ReflectorA,
+  RotorI,
+  RotorII,
+  RotorIII
+);
 
 let plainText =
-  "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like";
+  "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like";
 
 let cipher = machine.encryptPhrase(plainText);
 console.log(cipher);
 machine.reset();
+console.log("reset machine for decryption");
 let decipher = machine.encryptPhrase(cipher);
 console.log(decipher);
